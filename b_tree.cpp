8905872d4871966a16e0d600f899c5bce27f7b10
Utkarsh <utkarsh.yadav5@gmail.com>
2017-06-08 15:46:23 +0530

    Binary Tree 40 gfg questions

--- b_tree.cpp
+++ b_tree.cpp
@@ -294,6 +294,224 @@ public:
         return root;
     }
 
+    void doubleTree(Node* root){
+        if(root==NULL)
+            return;
+
+        doubleTree(root->left);
+        doubleTree(root->right);
+        Node* newNode=CreateNode(root->data);
+        Node* temp=root->left;
+        root->left=newNode;
+        newNode->left=temp;
+    }
+
+    int maxWidth(Node* root){
+        if(root==NULL)
+            return 0;
+
+        int result=0;
+
+        queue<Node* > q;
+        q.push(root);
+        while(!q.empty()){
+
+            int count=q.size();
+
+            result=max(count,result);
+
+            while(count--){
+                Node* temp=q.front();
+                q.pop();
+
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+
+        }
+        return result;
+    }
+
+    bool isFoldableUtil(Node* a,Node* b){
+        if(a==NULL && b==NULL)
+            return true;
+
+        if(a==NULL || b==NULL)
+            return false;
+
+        return isFoldableUtil(a->left,b->right) &&
+                    isFoldableUtil(a->right,b->left);
+    }
+
+    bool isFoldable(Node* root){
+        if(root==NULL)
+            return true;
+
+        return isFoldableUtil(root->left,root->right);
+    }
+
+    void printKdist(Node* root,int k){
+        int level=0;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int count=q.size();
+
+            if(level==k){
+                while(count--){
+                    cout<<q.front()->data<<" ";
+                    q.pop();
+                }
+                return;
+            }
+            level++;
+            while(count--){
+                Node* temp=q.front();
+                q.pop();
+
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+            }
+        }
+    }
+
+    void printKDist(Node* root,int k){
+        if(root==NULL)
+            return;
+
+        if(k==0){
+            cout<<root->data<<" ";
+            return;
+        }
+        else{
+            printKDist(root->left,k-1);
+            printKDist(root->right,k-1);
+        }
+    }
+
+    int level(Node* root,int key,int l){
+        if(root==NULL)
+            return 0;
+
+        if(root->data==key){
+            return l;
+        }
+        else{
+            int lvl;
+            lvl=level(root->left,key,l+1);
+            if(lvl!=0)
+                return lvl;
+            lvl=level(root->right,key,l+1);
+            return lvl;
+        }
+    }
+
+    bool ancestors(Node* root,int key){
+        if(root==NULL)
+            return false;
+
+        if(root->data==key)
+            return true;
+
+        if(ancestors(root->left,key) || ancestors(root->right,key)){
+            cout<<root->data<<" ";
+            return true;
+        }
+
+        return false;
+    }
+
+    bool isLeaf(Node* root){
+        if(root->left==NULL && root->right==NULL)
+            return true;
+
+        return false;
+    }
+
+    bool isSumTree(Node* root){
+        int ls,rs;
+
+        if(root==NULL || isLeaf(root))
+            return true;
+
+        if(isSumTree(root->left) && isSumTree(root->right)){
+
+            if(root->left==NULL)
+                ls=0;
+            else if(isLeaf(root->left))
+                ls=root->left->data;
+            else
+                ls=2*(root->left->data);
+
+            if(root->right==NULL)
+                ls=0;
+            else if(isLeaf(root->right))
+                ls=root->right->data;
+            else
+                ls=2*(root->right->data);
+
+            return (ls+rs==root->data);
+
+        }
+        return false;
+    }
+
+    bool areIdentical(Node* t1,Node* t2){
+        if(t1==NULL && t2==NULL)
+            return true;
+
+        if(t1==NULL || t2==NULL)
+            return false;
+
+        return ((t1->data==t2->data) && areIdentical(t1->left,t2->left)
+            && areIdentical(t1->right,t2->right));
+    }
+
+    bool isSubTree(Node* t1,Node* t2){
+        if(t2==NULL)
+            return true;
+
+        if(t1==NULL)
+            return false;
+
+        if(areIdentical(t1,t2))
+            return true;
+
+        return isSubTree(t1->left,t2)||isSubTree(t1->right,t2);
+    }
+
+    int toSumTree(Node* root){
+        if(root==NULL)
+            return 0;
+
+        int old_val=root->data;
+
+        root->data=toSumTree(root->left)+toSumTree(root->right);
+        return old_val+root->data;
+    }
+
+    unordered_map<int,int> myMap;
+
+    void verticalSum(Node* root,int dist){
+        if(root==NULL)
+            return;
+
+        verticalSum(root->left,dist-1);
+
+        if(myMap.find(dist)==myMap.end())
+            myMap.insert(make_pair(dist,root->data));
+        else
+            myMap[dist]=myMap[dist]+root->data;
+
+        verticalSum(root->right,dist+1);
+    }
+
 
 int main(){
     Node* root=CreateNode(1);
@@ -302,12 +520,14 @@ int main(){
     root->left->left=CreateNode(4);
     root->left->right=CreateNode(5);
 
-    Node* root2=CreateNode(10);
-    root2->left=CreateNode(8);
-    root2->right=CreateNode(2);
-    root2->left->left=CreateNode(3);
+    Node* root2=CreateNode(1);
+    root2->left=CreateNode(2);
+    root2->right=CreateNode(3);
+    root2->left->left=CreateNode(4);
     root2->left->right=CreateNode(5);
-    root2->right->left=CreateNode(2);
+    root2->right->left=CreateNode(8);
+    //root2->right->right->left=CreateNode(6);
+    root2->left->left->left=CreateNode(7);
 
     PreOrder(root);
     cout<<endl;
@@ -321,26 +541,38 @@ int main(){
     MorrisTraversal(root);
     cout<<endl;
     cout<<size(root)<<endl;
-    cout<<identical(root,root2)<<endl;
+    //cout<<identical(root,root2)<<endl;
     cout<<height(root)<<endl;
-    cout<<height(root2)<<endl;
+    //cout<<height(root2)<<endl;
     vector<int> v;
     root2leaf(root,v);
     //printCircular(Tree2List(root));
-    cout<<childrenSum(root2)<<endl;
+    //cout<<childrenSum(root2)<<endl;
     cout<<hasPathSum(root,100)<<endl;
 
-    int inOrder[6],preOrder[6];
+    // int inOrder[6],preOrder[6];
 
-    for(int i=0;i<6;i++)
-        cin>>preOrder[i];
+    // for(int i=0;i<6;i++)
+    //     cin>>preOrder[i];
 
-    for(int i=0;i<6;i++)
-        cin>>inOrder[i];
+    // for(int i=0;i<6;i++)
+    //     cin>>inOrder[i];
 
-    Node* root3=buildTree(inOrder,preOrder,0,5);
-    InOrder(root3);
-    cout<<endl;
+    // Node* root3=buildTree(inOrder,preOrder,0,5);
+    // InOrder(root3);
+    // cout<<endl;
+    //doubleTree(root);
+    //levelOrderPrint(root);
+    //cout<<maxWidth(root2)<<endl;
+    //cout<<isFoldable(root2)<<endl;
+    //printKDist(root2,2);
+    //cout<<endl;
+    //cout<<level(root,4,1)<<endl;
+    //cout<<ancestors(root2,3)<<endl;
+
+    verticalSum(root2,0);
+    for(int i=(-3);i<=2;i++)
+        cout<<i<<" : "<<myMap[i]<<endl;
     return 0;
 }
 
