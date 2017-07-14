cd9144ba3e6f3ae7792d0e9a232417de85fa39ab
Utkarsh <utkarsh.yadav5@gmail.com>
2017-07-14 16:48:22 +0530

    Strings Permutation with repeated char

--- strings/permutation_with_repeated.cpp
+++ strings/permutation_with_repeated.cpp
@@ -2,32 +2,30 @@
 
 using namespace std;
 
-void allLexicographicRecur(char *str,char *data,int last,int index){
-    int len=strlen(str);
+void permuteUtil(char *str,char *output,int index,int n){
+    if(index==n){
+        cout<<output<<endl;
+        return;
+    }
 
-    for(int i=0;i<len;i++){
-
-        data[index]=str[i];
-
-        if(index==last)
-            cout<<data<<endl;
-        else
-            allLexicographicRecur(str,data,last,index+1);
+    for(int i=0;i<n;i++){
+        output[index]=str[i];
+        permuteUtil(str,output,index+1,n);
     }
 }
 
-void allLexicographic(char *str){
+void permute(char *str){
     int len=strlen(str);
 
-    char *data = new char[len];
-    data[len]='\0';
+    char *output = new char[len+1];
+    output[len]='\0';
     sort(str,str+len);
 
-    allLexicographicRecur(str,data,len-1,0);
+    permuteUtil(str,output,0,len);
 }
 
 int main(){
     char str[] = "ABC";
     printf("All permutations with repetition of string are: \n");
-    allLexicographic(str);
+    permute(str);
 }
