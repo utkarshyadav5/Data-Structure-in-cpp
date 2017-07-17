1a27fce00ef39507d288687425df4cd78a8aed74
Utkarsh <utkarsh.yadav5@gmail.com>
2017-07-17 15:29:02 +0530

    DP min cost path with all three methods

--- dp/min_cost.cpp
+++ dp/min_cost.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// Recursive Method
+
 // int min_cost(int arr[3][3],int m,int n){
 //     if(n<0 || m<0)
 //         return INT_MAX;
@@ -12,6 +14,32 @@ using namespace std;
 //         return arr[m][n]+min(min(min_cost(arr,m-1,n-1),min_cost(arr,m,n-1)),
 //                     min_cost(arr,m-1,n));
 // }
+
+// Tabular method - Bottom to top
+
+// int minCost(int cost[R][C],int m,int n){
+//     int minPath[R][C];
+
+//     for(int i=0;i<=m;i++){
+//         for(int j=0;j<=n;j++){
+//             if(i==0 && j==0)
+//                 minPath[i][j]=cost[i][j];
+//             else
+//                 minPath[i][j]=cost[i][j]+min(
+//                                          min(minPath[i-1][j],minPath[i][j-1]),
+//                                              minPath[i-1][j-1]);
+//         }
+//     }
+
+//     for(int i=0;i<=m;i++){
+//         for(int j=0;j<=n;j++)
+//             cout<<minPath[i][j]<<" ";
+//         cout<<endl;
+//     }
+//     return minPath[m][n];
+// }
+
+// Memoization Method - Top to Bottom
 
 int dp[3][3];
 
