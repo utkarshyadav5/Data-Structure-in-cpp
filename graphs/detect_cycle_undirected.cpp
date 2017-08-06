e74f1f13d838190dca044a8272fb8d5d7ff71ac2
Utkarsh <utkarsh.yadav5@gmail.com>
2017-08-06 14:07:32 +0530

    Graph detect cycle in undirected graph

--- graphs/detect_cycle_undirected.cpp
+++ graphs/detect_cycle_undirected.cpp
@@ -1,105 +1,65 @@
 #include <bits/stdc++.h>
-#define MAX 1e4
+#define iPair pair<int,int>
 
 using namespace std;
 
 class Graph{
     int V;
-    vector<int> *adj;
+    int E;
+    vector<iPair> adj;
 public:
-    bool *visited;
-    bool *recStack;
-    int *color;
-    enum Color{WHITE,GREY,BLACK};
+    vector<int> arr;
 
     Graph(int vertices){
         V=vertices;
-        adj= new vector<int>[V];
-        visited = new bool[V];
-        recStack = new bool[V];
-        color = new int[V];
+        E=0;
+        for(int i=0;i<V;i++)
+            arr.push_back(i);
     }
 
     void addEdge(int u,int v){
-            adj[u].push_back(v);
+            adj.push_back(make_pair(u,v));
     }
 
     void print(){
-        for(int i=0;i<V;i++){
-            cout<<i<<" : ";
-            for(int j=0;j<adj[i].size();j++)
-                cout<<adj[i][j]<<" ";
-            cout<<endl;
-        }
+        cout<<adj.size()<<endl;
+        for(int i=0;i<adj.size();i++)
+            cout<<adj[i].first<<" "<<adj[i].second<<endl;
     }
 
-    ///////////////// - Method 1 - ////////////////////////////
-
-    // bool isCyclic_Util(int u){
-    //     if(visited[u]==false){
-    //         visited[u]=true;
-    //         recStack[u]=true;
-
-    //         for(int i=0;i<adj[u].size();i++){
-    //             int v=adj[u][i];
-    //             if(!visited[v] && isCyclic_Util(v))
-    //                 return true;
-    //             else if(recStack[v])
-    //                 return true;
-    //         }
-    //     }
-    //     recStack[u]=false;
-    //     return false;
-    // }
-
-    // bool isCyclic(){
-    //     for(int i=0;i<V;i++){
-    //         visited[i]=false;
-    //         recStack[i]=false;
-    //     }
-
-    //     for(int i=0;i<V;i++)
-    //         if(isCyclic_Util(i))
-    //             return true;
-
-    //     return false;
-    // }
-
-    ////////////////////////////////////////////////////////
-
-    ///////////////////// - Method 2 - /////////////////////
-
-    bool isCyclic_Util(int u){
-
-        if(color[u]==WHITE){
-            color[u]=GREY;
-
-            for(int i=0;i<adj[u].size();i++){
-                int v=adj[u][i];
-
-                if(color[v]==GREY)
-                    return true;
-
-                if(color[v]==WHITE && isCyclic_Util(v))
-                    return true;
-            }
+    int root(int i){
+        while(i!=arr[i]){
+            arr[i]=arr[arr[i]];
+            i=arr[i];
         }
-        color[u]=BLACK;
-        return false;
+        return i;
+    }
+
+    void union_(int a,int b){
+        int root_a=root(a);
+        int root_b=root(b);
+        arr[root_a]=root_b;
+    }
+
+    bool find(int a,int b){
+        if(root(a)==root(b))
+            return true;
+        else
+            return false;
     }
 
     bool isCyclic(){
-        for(int i=0;i<V;i++)
-            color[i]=WHITE;
-
-        for(int i=0;i<V;i++)
-            if(isCyclic_Util(i))
+        for(int i=0;i<adj.size();i++){
+            int u=adj[i].first;
+            int v=adj[i].second;
+            if(find(u,v))
                 return true;
-
+            union_(u,v);
+        }
         return false;
     }
 
-    ////////////////////////////////////////////////////////
+
 
 };
 
@@ -111,12 +71,11 @@ int main(){
     g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(3, 3);
-
+    //g.print();
     if(g.isCyclic())
         cout << "Graph contains cycle"<<endl;
     else
         cout << "Graph doesn't contain cycle"<<endl;
-    return 0;
     return 0;
 }
 
